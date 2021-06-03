9e920c1e9c11309868a6ea44c51693fbbcbe2a9d
Josh Luria <josh.luria@gmail.com>
2021-06-03 16:00:23 -0400

    more arbitrary IDE change

--- proj3-bucket-sort-starter/src/app/IntegerBucketSorter.java
+++ proj3-bucket-sort-starter/src/app/IntegerBucketSorter.java
@@ -19,7 +19,7 @@ public class IntegerBucketSorter implements Sorter {
     * Use the bucket sort algorithm to return an array of the integers in the input
     * array in sorted order. The input array and the returned array may be the same
     * array.
-    * 
+    *
     * @param dataArray an array of integers to be sorted.
     * @return an array of integers sorted in ascending order.
     * @throws an Exception if any integer in the array is > MAX_DIGIT_LIMIT.
@@ -36,7 +36,7 @@ public class IntegerBucketSorter implements Sorter {
     * place. The integer's value at the current place is the index of the row into
     * which it is distributed. The next available cell in the row has to be found
     * so data that already exists in that row is not overwritten.
-    * 
+    *
     * @param dataArray the integers to be sorted.
     * @param curPlace  the current "place" used to determine the bucket where an
     *                  integer is written to.
@@ -50,7 +50,7 @@ public class IntegerBucketSorter implements Sorter {
     * Collection phase: Iterate through the buckets array starting at row 0.
     * Collect all integers stored in that row into the data array, in the order
     * they appear in that row. Do that for each row until done.
-    * 
+    *
     * @param dataArray the integers to be sorted.
     */
    public void collect(int[] dataArray) {
@@ -63,7 +63,7 @@ public class IntegerBucketSorter implements Sorter {
     * array. This is used to determine how many iterations of the bucket sort
     * algorithm are required. Assume the length of the array >=0. This method calls
     * findIntLength.
-    * 
+    *
     * @param array the input array of integers to be sorted.
     * @throws Exception if findIntLength throws an Exception.
     * @return the largest number of digits found in the integers in the array,
@@ -82,7 +82,7 @@ public class IntegerBucketSorter implements Sorter {
     * the return would be 2. If num was 500, the return would be 3. etc. This
     * method should handle an integer with up to MAX_DIGIT_LIMIT places. Assume num
     * >=0.
-    * 
+    *
     * @param num the integer whose number of digits we want to determine.
     * @return the number of digits of num.
     * @throws Exception if the number of digits of num is > MAX_DIGIT_LIMIT.
@@ -100,7 +100,7 @@ public class IntegerBucketSorter implements Sorter {
     * place was 1 and num was 5, the return would be 5. If place was 2 and num was
     * 5, the return would be 0. If place was 1 and num was 39, the return would be
     * 9. If place was 2 and num was 167, the return would be 6. Assume num >=0.
-    * 
+    *
     * @param place the specific digit required.
     * @param num   the integer we want to extract the digit from.
     * @return the digit at the specified place.
