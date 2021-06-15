4a5101ddf8cd14cbf7f630dfb711f938b4d96a0a
Josh Luria <josh.luria@gmail.com>
2021-06-15 15:34:54 -0400

    remove completed TODO from negate operator

--- proj_postfix_eval_starter/src/language/arith/NegateOperator.java
+++ proj_postfix_eval_starter/src/language/arith/NegateOperator.java
@@ -9,11 +9,6 @@ import language.UnaryOperator;
  */
 
 public class NegateOperator extends UnaryOperator<Integer> {
-  // TODO: You will notice that this class extends BinaryOperator.
-  // That is not correct as negate is a unary operator. You should first
-  // write an abstract class called UnaryOperator, paralleling
-  // BinaryOperator, that abstracts out all the bits common
-  // across UnaryOperators.
   @Override
   public Operand<Integer> performOperation() {
     if (op0 == null)
