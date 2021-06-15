f1899f188e65e9e1eb2836e34b8ad7e07caa4ae1
Josh Luria <josh.luria@gmail.com>
2021-06-15 14:56:35 -0400

    subtraction operator returns the difference between two values

--- proj_postfix_eval_starter/src/language/arith/SubOperator.java
+++ proj_postfix_eval_starter/src/language/arith/SubOperator.java
@@ -4,17 +4,20 @@ import language.BinaryOperator;
 import language.Operand;
 
 /**
- * The {@code SubOperator} is an operator that performs subtraction on two integers.
+ * The {@code SubOperator} is an operator that performs subtraction on two
+ * integers.
  */
 public class SubOperator extends BinaryOperator<Integer> {
   /** {@inheritDoc} */
   @Override
   public Operand<Integer> performOperation() {
-    // TODO: Before you get started, have you looked at the
-    // PlusOperator class? You'll notice that it is taking advantage
-    // of the abstract BinaryOperator class. Take a moment to
-    // also look at that class. Finally, you should implement
-    // this class.
-    return null;
+    if (op0 == null || op1 == null)
+      throw new IllegalStateException("Could not perform subtraction operation prior to operands being set.");
+    Integer result = op0.getValue() - op1.getValue();
+    return new Operand<Integer>(result);
+  }
+
+  public String toString() {
+    return "-";
   }
 }
